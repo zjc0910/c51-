7a966231a9859fc9743c4a5788beec3a48a186b4
zjc0910 <3518327029@qq.com>
2025-04-21 12:44:33 +0800

    Add files via upload

---
+++ LED闪烁/main.c
@@ -0,0 +1,41 @@
+#include <REGX52.H>
+#include <INTRINS.H>
+void Delay500ms()		//@12.000MHz
+{
+	unsigned char i, j, k;
+
+	_nop_();
+	i = 4;
+	j = 205;
+	k = 187;
+	do
+	{
+		do
+		{
+			while (--k);
+		} while (--j);
+	} while (--i);
+}
+
+void main(){
+	while(1)
+	{
+		 P2=0xfe;
+		 Delay500ms();
+		 P2=0xff;
+		 Delay500ms();
+		 
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
